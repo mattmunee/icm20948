71a6a9849b6f4e041bcae3ad757a440ceb43acfb
Matt Reaves <matt.reaves@jhuapl.edu>
2019-10-30 13:31:44 -0400

    still working

--- Include/Icm20948Device.hpp
+++ Include/Icm20948Device.hpp
@@ -1,11 +1,39 @@
 #ifndef ICM_20948_DEVICE_HPP
 #define ICM_20948_DEVICE_HPP
 
-class DecisionTree {  // <----- 'class' (not 'namespace')
-public:  // <------ access specifier
+#include "ICM_20948_REGISTERS.h"
+#include <string>
+
+typedef enum {
+	SUCCESS,
+	FAILED_TO_OPEN_ADAPTER,
+	FAILED_TO_TALK_TO_SLAVE,
+	FAILED_TO_READ_WORD_DATA
+}Icm20948ErrorCodes;
+
+class Icm20948Device {
+public:
+	
+
+	Icm20948Device(
+		unsigned short adapter_number,
+		long device_address);
 	static double Entropy(int pos, int neg);
+
+	Icm20948ErrorCodes whoAmI(ICM_20948_WHO_AM_I_t& out_t);
+
 private:
-	int i;
+	unsigned short adapter_number_;
+	long device_address_;
+	int device_file_;
+
+
+	Icm20948ErrorCodes openDevice();
+
+	Icm20948ErrorCodes readRegister(
+		unsigned short user_bank, 
+		ICM_20948_REGISTER_MAP register_name,
+		__s32& data);
 };
 
 #endif
--- Source/main.cpp
+++ Source/main.cpp
@@ -7,60 +7,14 @@
 #include "ICM_20948_REGISTERS.h"
 #include "Icm20948Device.hpp"
 
-ICM_20948_WHO_AM_I_t getWhoAmI(unsigned short adapter_number, long device_address){
-	int file;
-    char filename[20];
-
-    snprintf(filename, 19, "/dev/i2c-%d", adapter_number);
-    if ((file = open(filename,O_RDWR)) < 0) {
-        printf("Failed to open the bus.");
-        /* ERROR HANDLING; you can check errno to see what went wrong */
-        exit(1);
-    }
-    if (ioctl(file,I2C_SLAVE,device_address) < 0) {
-        printf("Failed to acquire bus access and/or talk to slave.\n");
-        /* ERROR HANDLING; you can check errno to see what went wrong */
-        exit(1);
-    }
-	__u8 reg = REG_WHO_AM_I; /* Device register to access */
-	__s32 res;
-	ICM_20948_WHO_AM_I_t res_t;
-	
-	char buf[10];
-	printf("Size of ICM_20948_WHO_AM_I_t: %d",sizeof(ICM_20948_WHO_AM_I_t));
-	/* Using SMBus commands */
-	res = i2c_smbus_read_word_data(file, reg);
-	if (res < 0) {
-		/* ERROR HANDLING: i2c transaction failed */
-	} else {
-		printf("Result at register %02x: %02x\n",reg,res);
-		res_t.WHO_AM_I = (uint8_t)((res >> WHO_AM_I_BIT_INDEX) & WHO_AM_I_BIT_MASK);
-		return res_t;
-	}
-}
 /* main.c */
 int main(int argc, char *argv[]) {
     printf("Hello\n");
 
-    int file;
-    int adapter_nr = 1; /* probably dynamically determined */
-    int addr = 0x68;
-    char filename[20];
-
-    snprintf(filename, 19, "/dev/i2c-%d", adapter_nr);
-    if ((file = open(filename,O_RDWR)) < 0) {
-        printf("Failed to open the bus.");
-        /* ERROR HANDLING; you can check errno to see what went wrong */
-        exit(1);
-    }
-    if (ioctl(file,I2C_SLAVE,addr) < 0) {
-        printf("Failed to acquire bus access and/or talk to slave.\n");
-        /* ERROR HANDLING; you can check errno to see what went wrong */
-        exit(1);
-    }
-    
-	ICM_20948_WHO_AM_I_t whoAmI = getWhoAmI(adapter_nr,addr);
-	printf("Result of Who Am I?: %02x\n",whoAmI.WHO_AM_I);
+	Icm20948Device dev(1,0x68);
+	ICM_20948_WHO_AM_I_t whoIsDev;
+	Icm20948ErrorCodes success = dev.whoAmI(whoIsDev);
+	printf("Result of Who Am I?: %02x\n", whoIsDev.WHO_AM_I);
 	
     return 0;
 }
