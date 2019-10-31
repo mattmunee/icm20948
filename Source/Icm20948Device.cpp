a26656777ac2c77c4e04edb35022a928bb22868e
Matt Reaves <matt.reaves@jhuapl.edu>
2019-10-31 15:52:49 -0400

    a

--- Source/Icm20948Device.cpp
+++ Source/Icm20948Device.cpp
@@ -458,6 +458,8 @@ Icm20948ErrorCodes Icm20948Device::goToLowPowerWomMode(
 	ICM_20948_WOM_ALGORITHM algorithm)
 {
 	Icm20948ErrorCodes success;
+
+	debugStream_ << "disableTempSensor:" << std::endl;
 	success = disableTempSensor(true);
 	if (SUCCESS != success) {
 		debugStream_ << "Failed to disableTempSensor!" << std::endl;
@@ -484,14 +486,7 @@ Icm20948ErrorCodes Icm20948Device::goToLowPowerWomMode(
 		debugStream_ << "Failed to enableAccelDutyCycle!" << std::endl;
 		return success;
 	}*/
-	
-	/*debugStream_ << "enableLowPowerMode:" << std::endl;
-	success = enableLowPowerMode(true);
-	if (SUCCESS != success) {
-		debugStream_ << "Failed to enableLowPowerMode!" << std::endl;
-		return success;
-	}*/
-	
+
 	debugStream_ << "setWomThreshold:" << std::endl;
 	success = setWomThreshold(wom_threshold_mg);
 	if (SUCCESS != success) {
@@ -503,6 +498,13 @@ Icm20948ErrorCodes Icm20948Device::goToLowPowerWomMode(
 	success = enableWomLogic(true, algorithm);
 	if (SUCCESS != success) {
 		debugStream_ << "Failed to enableWomLogic!" << std::endl;
+		return success;
+	}
+
+	debugStream_ << "enableLowPowerMode:" << std::endl;
+	success = enableLowPowerMode(true);
+	if (SUCCESS != success) {
+		debugStream_ << "Failed to enableLowPowerMode!" << std::endl;
 		return success;
 	}
 
